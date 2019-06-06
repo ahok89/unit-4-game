92c3e578e99d03db7eed8a780e68595497b4a8e8
Andrew Hok <ahok89@gmail.com>
2019-06-05 22:18:13 -0400

    added js components

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 var targetNumber = Math.floor(Math.random()*120)+1;
 
   $("#number-to-guess").text(targetNumber);
@@ -7,5 +8,32 @@ var targetNumber = Math.floor(Math.random()*120)+1;
   var numberOptions = [10, 5, 3, 7];
 
   for (var i = 0; i < numberOptions.length; i++) {
+// 
+    var imageCrystal = $("<img>");
 
+    imageCrystal.addClass("crystal-image");
+
+    imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
+
+    imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+
+    $("#crystals").append(imageCrystal);
+}
+
+$(".crystal-image").on("click", function() {
     
+    var crystalValue = ($(this).attr("data-crystalvalue"));
+    
+    crystalValue = parseInt(crystalValue);
+
+    counter += crystalValue;
+
+    alert("New score: " + counter);
+
+    if (counter === targetNumber) {
+      alert("You win!");
+    }
+
+    else if (counter >= targetNumber) {
+      alert("You lose!!");
+    }
