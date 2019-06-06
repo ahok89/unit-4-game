c15f0cb8bbbd5806ea526594f733d2628d56566b
Andrew Hok <ahok89@gmail.com>
2019-06-05 21:47:56 -0400

    added js game mechanics

---
+++ main.js
@@ -0,0 +1,11 @@
+var targetNumber = Math.floor(Math.random()*120)+1;
+
+  $("#number-to-guess").text(targetNumber);
+
+  var counter = 0;
+
+  var numberOptions = [10, 5, 3, 7];
+
+  for (var i = 0; i < numberOptions.length; i++) {
+
+    
